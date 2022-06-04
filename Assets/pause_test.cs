f3f8e27f413cf23dcea5ca9fa0fae716ca14d5cf
Jerald Gau <jerald.gau87@gmail.com>
2022-06-04 16:58:58 +0800

    Pause Button prefab and Scenario additions
    - Changed the pause buttons in the Scenario and Explanation scenes into prefabs to make future additions to the pause button function easier and more streamlined
    - Added the fourth set of scenarios together with their explanations

---
+++ Assets/pause_test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause_test : MonoBehaviour
+{
+    public void button()
+    {
+        Debug.Log("button is pressed");
+    }
+}
