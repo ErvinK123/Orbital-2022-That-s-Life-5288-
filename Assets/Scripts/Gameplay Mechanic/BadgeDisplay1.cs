b6ea4f639dc6f9656458afcceefdde6958c5eafd
Jerald Gau <jerald.gau87@gmail.com>
2022-07-11 15:40:05 +0800

    Badge Popups
    Added popup descriptions for all the achievement badges when you unlock them.

--- Assets/Scripts/Gameplay Mechanic/BadgeDisplay1.cs
+++ Assets/Scripts/Gameplay Mechanic/BadgeDisplay1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BadgeDisplay1 : MonoBehaviour
 {
-    //Initialising the badge locks as GameObject variables
+    // Initialising the badge locks as GameObject variables
     public GameObject WorkJunkieLock;
     public GameObject SmoothBrainLock;
     public GameObject SocialButterflyLock;
@@ -15,12 +15,34 @@ public class BadgeDisplay1 : MonoBehaviour
     public GameObject HopelesslyIneptLock;
     public GameObject SaintLock;
 
+    // Initialising all the badge popups
+    public GameObject WorkJunkieDescription;
+    public GameObject SmoothBrainDescription;
+    public GameObject SocialButterflyDescription;
+    public GameObject ShutInDescription;
+    public GameObject PeakHumanDescription;
+    public GameObject ZombieDescription;
+    public GameObject HandymanDescription;
+    public GameObject HopelesslyIneptDescription;
+    public GameObject SaintDescription;
 
-    
+
+
     void Start()
     {
         Player.collector();
         displayBadge();
+
+        // Setting all the badge popups to be disabled on start
+        WorkJunkieDescription.SetActive(false);
+        SmoothBrainDescription.SetActive(false);
+        SocialButterflyDescription.SetActive(false);
+        ShutInDescription.SetActive(false);
+        PeakHumanDescription.SetActive(false);
+        ZombieDescription.SetActive(false);
+        HandymanDescription.SetActive(false);
+        HopelesslyIneptDescription.SetActive(false);
+        SaintDescription.SetActive(false);
     }
 
     public void displayBadge()
@@ -73,6 +95,96 @@ public class BadgeDisplay1 : MonoBehaviour
         {
             SaintLock.SetActive(false);
         }
+    }
+
+    public void workJunkiePopup()
+    {
+        WorkJunkieDescription.SetActive(true);
+    }
+
+    public void workJunkieClose()
+    {
+        WorkJunkieDescription.SetActive(false);
+    }
+
+    public void smoothBrainPopup()
+    {
+        SmoothBrainDescription.SetActive(true);
+    }
+
+    public void smoothBrainClose()
+    {
+        SmoothBrainDescription.SetActive(false);
+    }
+
+    public void socialButterflyPopup()
+    {
+        SocialButterflyDescription.SetActive(true);
+    }
+
+    public void socialButterflyClose()
+    {
+        SocialButterflyDescription.SetActive(false);
+    }
+
+    public void shutInPopup()
+    {
+        ShutInDescription.SetActive(true);
+    }
+
+    public void shutInClose()
+    {
+        ShutInDescription.SetActive(false);
+    }
+
+    public void peakHumanPopup()
+    {
+        PeakHumanDescription.SetActive(true);
+    }
+
+    public void peakHumanClose()
+    {
+        PeakHumanDescription.SetActive(false);
+    }
+
+    public void zombiePopup()
+    {
+        ZombieDescription.SetActive(true);
+    }
+
+    public void zombieClose()
+    {
+        ZombieDescription.SetActive(false);
+    }
+
+    public void handymanPopup()
+    {
+        HandymanDescription.SetActive(true);
+    }
+
+    public void handymanClose()
+    {
+        HandymanDescription.SetActive(false);
+    }
+
+    public void hopelesslyIneptPopup()
+    {
+        HopelesslyIneptDescription.SetActive(true);
+    }
+
+    public void hopelesslyIneptClose()
+    {
+        HopelesslyIneptDescription.SetActive(false);
+    }
+
+    public void saintPopup()
+    {
+        SaintDescription.SetActive(true);
+    }
+
+    public void saintClose()
+    {
+        SaintDescription.SetActive(false);
     }
     
 }
